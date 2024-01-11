0c75ce4a972d7e8b4d6840b9f57ceeac46e89bab
MaxCsharpCourse <ishkov.m@gmail.com>
2024-01-11 19:56:25 +0300

    nask 4.1.17

--- Program.cs
+++ Program.cs
@@ -4,16 +4,30 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
+        var color = Console.ReadLine();
 
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
     }
-}
